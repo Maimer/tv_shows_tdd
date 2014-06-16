19039256808729102bb8ceb48743f907d2c71067
Maimer <nesnl23@yahoo.com>
2014-06-16 14:40:35 -0400

    Added character controller

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,9 +1,23 @@
 class CharacterController < ApplicationController
   def new
-
+    @character = Character.new
   end
 
   def create
+    @character = Character.new(character_params)
 
+    if @character.save
+      flash[:notice] = "Success!"
+      redirect_to "/television_shows/#{params[:id]}"
+    else
+      flash.now[:notice] = "Your character couldn't be saved."
+      redirect_to "/television_shows/#{params[:id]}"
+    end
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :actor, :description)
   end
 end
